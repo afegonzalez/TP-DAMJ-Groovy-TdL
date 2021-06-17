8b68f1996b91365708da2e65fed0cef32fb7689c
Martina <larosamf@gmail.com>
2021-06-16 23:03:46 -0300

    Agrego estructura de la clase Pelicula. Se obtienen los datos de imdb

--- fiublix-app/server/grails-app/domain/fiublix/Movie.groovy
+++ fiublix-app/server/grails-app/domain/fiublix/Movie.groovy
@@ -1,7 +1,27 @@
 package fiublix
 
 class Movie {
+    //https://v2.sg.media-imdb.com/suggestion/t/titanic.json
+
+    String name
+    String image
+    List<String> protagonists
+    String director
+    String year
 
     static constraints = {
+        name blank: false, minSize: 1, maxSize: 150
+    }
+
+    def Movie(name) {
+        def url = "https://v2.sg.media-imdb.com/suggestion/t/${name}.json"
+
+        def json = new groovy.json.JsonSlurper().parseText(url.toURL().getText())
+
+        this.name = name
+        this.image = json['d'][0]['i']['imageUrl']
+        this.protagonists = json['d'][0]['s'].split(',')
+        this.director = json['d'][0]['v'][-1]['l']
+        this.year = json['d'][0]['y']
     }
 }
