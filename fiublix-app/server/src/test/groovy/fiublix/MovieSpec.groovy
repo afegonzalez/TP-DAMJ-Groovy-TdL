958e1b34f5e7bd19352f4341f690b9521d7b841b
Martina <larosamf@gmail.com>
2021-06-16 23:05:12 -0300

    Agrego test para creacion de Pelicula

--- fiublix-app/server/src/test/groovy/fiublix/MovieSpec.groovy
+++ fiublix-app/server/src/test/groovy/fiublix/MovieSpec.groovy
@@ -5,14 +5,11 @@ import spock.lang.Specification
 
 class MovieSpec extends Specification implements DomainUnitTest<Movie> {
 
-    def setup() {
-    }
+    void "Test 1: New movie"() {
+        setup:
+        def movie = new Movie('Titanic')
 
-    def cleanup() {
-    }
-
-    void "test something"() {
-        expect:"fix me"
-            true == false
+        expect:
+        movie.image == 'https://m.media-amazon.com/images/M/MV5BMDdmZGU3NDQtY2E5My00ZTliLWIzOTUtMTY4ZGI1YjdiNjk3XkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_.jpg'
     }
 }
