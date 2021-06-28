c8672d460b4a782360e46ba1f58ce4d5ff7ecb1c
Martina <larosamf@gmail.com>
2021-06-27 22:19:26 -0300

    Campos en nulo de la pelicula si no se encuentra en imdb

--- fiublix-app/server/grails-app/domain/fiublix/Movie.groovy
+++ fiublix-app/server/grails-app/domain/fiublix/Movie.groovy
@@ -11,17 +11,26 @@ class Movie {
 
     static constraints = {
         name blank: false, minSize: 1, maxSize: 150
+        image nullable: true
+        protagonists nullable: true
+        director nullable: true
+        year nullable: true
     }
 
     def Movie(name) {
         def url = "https://v2.sg.media-imdb.com/suggestion/t/${name}.json"
-
-        def json = new groovy.json.JsonSlurper().parseText(url.toURL().getText())
-
+        try {
+            def json = new groovy.json.JsonSlurper().parseText(url.toURL().getText())
+            movie.image = json['d'][0]['i']['imageUrl']
+            movie.protagonists = json['d'][0]['s'].split(',')
+            movie.director = json['d'][0]['v'][-1]['l']
+            movie.year = json['d'][0]['y']
+        } catch (IOException e) {
+            movie.image = "No disponible"
+            movie.director = "No disponible"
+            movie.year = "No disponible"
+        }
+        
         this.name = name
-        this.image = json['d'][0]['i']['imageUrl']
-        this.protagonists = json['d'][0]['s'].split(',')
-        this.director = json['d'][0]['v'][-1]['l']
-        this.year = json['d'][0]['y']
     }
 }
