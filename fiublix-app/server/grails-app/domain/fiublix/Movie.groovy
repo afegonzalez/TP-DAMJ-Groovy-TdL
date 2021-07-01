e96642b9db2bc3896cbd373a5646980eec43104e
Martina <larosamf@gmail.com>
2021-06-30 22:17:47 -0300

    Borro codigo repetido

--- fiublix-app/server/grails-app/domain/fiublix/Movie.groovy
+++ fiublix-app/server/grails-app/domain/fiublix/Movie.groovy
@@ -9,28 +9,14 @@ class Movie {
     String director
     String year
 
+    static hasMany = [user: User]
+    static belongsTo = [user: User]
+
     static constraints = {
         name blank: false, minSize: 1, maxSize: 150
         image nullable: true
         protagonists nullable: true
         director nullable: true
         year nullable: true
-    }
-
-    def Movie(name) {
-        def url = "https://v2.sg.media-imdb.com/suggestion/t/${name}.json"
-        try {
-            def json = new groovy.json.JsonSlurper().parseText(url.toURL().getText())
-            movie.image = json['d'][0]['i']['imageUrl']
-            movie.protagonists = json['d'][0]['s'].split(',')
-            movie.director = json['d'][0]['v'][-1]['l']
-            movie.year = json['d'][0]['y']
-        } catch (IOException e) {
-            movie.image = "No disponible"
-            movie.director = "No disponible"
-            movie.year = "No disponible"
-        }
-        
-        this.name = name
     }
 }
